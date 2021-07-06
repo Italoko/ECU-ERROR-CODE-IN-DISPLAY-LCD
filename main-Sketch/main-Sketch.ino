6b317e7e02472f0e16e3611d8cb6ebd0c7e09bd5
ITALO PIOVAN <italo_piovan@hotmail.com>
2021-07-06 20:05:15 -0300

    Implement functions
    -Configuração do LCD KEYPAD SHIELD
    -Configuração tabela de erros
    -Função p/ imprimir erro
    -Funcionalidade dos botões
    -Novo caractere "seta p/ baixo"

--- main-Sketch/main-Sketch.ino
+++ main-Sketch/main-Sketch.ino
@@ -1,34 +1,62 @@
 #include <LiquidCrystal.h>
 
-                  //Pinos de conexão LCD
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
+byte arrowDown[8]={ B00100,B00100,B00100,B00100,B00100,B10101,B01110,B00100}; // Seta p/ baixo
 
-//definição do pino de entrada
-#define input 13
-//definição em ms bounce do btn
-#define bounce 50
+// *** INPUT SETTINGS ***
 
-/*
-    TABELA DE ERROS
-    INDICE  = QUANTIDADE DE PULSOS
-*/
-char *tabError[] = 
-{
-  "ERROR 01", 
-  "ERROR 02", 
-  "ERROR 03",                   
-  "ERROR 04", 
-  "ERROR 05", 
-  "ERROR 06"
-};
+#define pinPulse 2
+//or #define pinPulse 1
 
-//Variável contadora de pulsos
-int count;
+//**********************************
 
-//Config Botão
-bool stateBtn = false;
-bool stateBtnAnt = false;
+// *** LCD KEYPAD SHIELD SETTINGS ***
+
+#define pinButtons A0
+#define pinBackLight 10
+              // RS,EN,D4,D5,D6,D7
+LiquidCrystal lcd(8,9,4,5,6,7);
+
+//**********************************
+
+// ******* BUTTON SETTINGS *********
+
+#define btnNONE   0
+#define btnSELECT 1
+#define btnLEFT   2
+#define btnUP     3
+#define btnDOWN   4
+#define btnRIGHT  5
+#define tempBounce 50
+
 unsigned long delayBtn;
+int estBtnAnt = btnNONE;
+//**********************************
+
+// ***** DIAGNOSIS SETTINGS ********
+
+#define TL 15 // Qtd de diagnosticos 
+int count = 0; //Variável contadora de pulsos
+
+int codeError[TL]   = {0,12,13,14,15,16,22,28,30,33,39,41,44,46,50}; //Codigo do erro
+
+String msgError[TL] = {"ERRO NAO IDENTIFICADO",
+"Nenhum sinal recebido do sensor de posicao do virabrequim",
+"Sensor da pressao do ar de admissao:circuito aberto ou curto circuito detectado",
+"Sensor de pressao do ar de admissao defeituoso",
+"Sensor de posicao do acelerador TPS aberto ou curto-circuito",
+"TPS esta preso agarrado",
+"Sensor da temperatura do ar de admissao - circuito aberto ou curto circuito detectado",
+"Sensor da temperatura do motor - circuito aberto ou curto circuito detectado"
+"A motocicleta caiu",
+"Circuito aberto detectado no enrolamento primario da bobina de ignicao",
+"Circuito aberto ou curto circuito detectado no injetor de combustivel",
+"Sensor de inclinacao com o circuito aberto ou curto circuito",
+"Erro detectado durante a leitura ou gravacao E2PROM",
+"Fornecimento de energia para o sistema FI nao esta normal",
+"Memoria da ECU defeituosa",
+};//Msg sintoma do erro
+
+//**********************************
 
 //Config tela inicial
 void initScreen()
@@ -38,43 +66,109 @@ void initScreen()
   lcd.print("PIOVAN MOTOS");
 }
 
-void printError(int err)
+void init()
 {
-  lcd.clear();
-  lcd.print(tabError[err]);
-  delay(2500);
-  count = 0 ;
-  initScreen();
-}
-
-void setup() 
-{
-  //Config pino de entrada
-  pinMode(input, INPUT_PULLUP);
-  
-  //Config LCD DYSPLAY (16COL X 2LIN)
   lcd.begin(16, 2);
+  lcd.createChar(1,arrowDown);
+  pinMode(pinBackLight, OUTPUT);
+  digitalWrite(pinBackLight, HIGH);
   
+  pinMode(pinPulse, INPUT);
   count = 0;
   initScreen();
 }
 
+int opButtons(int btnValue)
+{
+  if ((btnValue < 800) && (btnValue >= 600)) 
+     return btnSELECT;
+   else if ((btnValue < 600) && (btnValue >= 400)) 
+     return btnLEFT;
+   else if ((btnValue < 400) && (btnValue >= 200)) 
+     return btnUP;
+   else if ((btnValue < 200) && (btnValue >= 60))
+     return btnDOWN;
+   else if  (btnValue < 60)
+     return btnRIGHT;
+   else 
+     return btnNONE;
+}
+
+void actionBtn(int btn)
+{
+  if ((millis() - delayBtn) > tempBounce)
+   if ((btn == btnNONE) && (estBtnAnt != btnNONE)) 
+   {
+      //******* ACTION HERE ( PRINT OR OTHER FUNCTION )  ******
+      delayBtn = millis();
+   }
+  estBtnAnt = btn;
+}
+
+int searchCodeError(int cod)
+{
+  int i=0;
+  while(i < TL && cod < codeError[i]) 
+    i++;
+    
+  if(cod == codeError[i])
+    return i;
+  return 0;  
+}
+
+void printError(int i)
+{
+  int btn;
+  String msg = msgError[i];
+  String line1,line2;
+  lcd.clear();
+  for(int i = 0 ; i < msg.length(); i++)
+  {
+    if(line1.length() < 16)
+      line1 += msg.charAt(i);
+    else
+      if(line2.length() < 14)
+        line2 += msg.charAt(i);  
+        
+    if(line1.length() < 16 && line2.length() < 14 || i >= msg.length())
+    {
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print(line1);
+
+      lcd.setCursor(0,1);
+      lcd.print(line2);
+
+      if(i < msg.length())
+      {
+        lcd.setCursor(15,1);
+        lcd.write(1);
+  
+        line1 = line2 = "";
+        delay(30000);
+        btn = analogRead(pinButtons);
+        while(btn == btnNONE)
+          btn = analogRead(pinButtons);
+        actionBtn(opButtons(btn));
+        btn = btnNONE;
+      }
+    }
+  }
+}
+
+void readError()
+{
+  
+}
+
+void setup() 
+{
+  init();
+}
+
 void loop() 
 {
-  stateBtn = !digitalRead(input);
-  if ((millis() - delayBtn) > bounce) 
-  {
-      if (stateBtn && !stateBtnAnt) 
-      {
-          count++;
-          if (count > 3) err = count;
-          delayBtn = millis();
-      }
-      else if (!stateBtn && stateBtnAnt) delayBtn = millis();
-  }
-  stateBtnAnt = stateBtn;
-  if(count > 1)
-    printError(err);
+  readError();
 }
  
  
